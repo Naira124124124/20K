52895eec541869bf200b164ab28e90802213dd69
Naira124124124 <159915449+Naira124124124@users.noreply.github.com>
2025-02-27 17:36:06 +0400

    Add files via upload

---
+++ Интерфейс регистрации/Авторизация-20250227T130058Z-001/Авторизация/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = textBox2.Text;
+
+            if (password.Length < 8)
+            {
+                MessageBox.Show("Неверный пароль! Пароль должен быть не менее 8 символов.", "Ошибка");
+            }
+            else
+            {
+                string filename = "password.txt"; // Имя файла
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), filename); // Путь к рабочему столу
+                File.WriteAllText(path, password); // Запись пароля в файл
+
+                MessageBox.Show("Авторизация прошла успешно!", "Успех");
+            }
+        }
+    }
+}
